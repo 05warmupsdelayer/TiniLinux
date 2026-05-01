6041219b74c6386bb806e8cc5e9cd1ed40bf3318
jon <05warmups.delayer@icloud.com>
2026-05-01 09:36:37 -0300

    Revert "Update simple-keymon.py"
    This reverts commit 10b2dd10ee347722dcd6ef9754445b066c244a51.

--- board/h700_rootrw/rootfs/usr/local/bin/simple-keymon.py
+++ board/h700_rootrw/rootfs/usr/local/bin/simple-keymon.py
@@ -3,6 +3,7 @@
 import evdev
 import asyncio
 import subprocess
+import time
 
 def find_device_by_name(name):
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
@@ -13,9 +14,14 @@ def find_device_by_name(name):
 
 joypadInput = find_device_by_name("H700 Gamepad")
 volumeInput = find_device_by_name("gpio-keys-volume")
-lidInput = find_device_by_name("gpio-keys-lid")
+powerKeyInput = find_device_by_name("axp20x-pek")
+# rg35xxspLid = find_device_by_name("gpio-keys-lid")
+devices = [joypadInput, volumeInput, powerKeyInput]
+
 brightness_path = "/sys/devices/platform/backlight/backlight/backlight/brightness"
-max_brightness = int(open("/sys/devices/platform/backlight/backlight/backlight/max_brightness").read())
+max_brightness = int(open("/sys/devices/platform/backlight/backlight/backlight/max_brightness", "r").read())
+
+suspended = 0
 
 class Joypad:
     l1 = 310
@@ -37,97 +43,71 @@ class Joypad:
     select = 314
     start = 315
 
-
-def runcmd(cmd):
+def runcmd(cmd, *args, **kw):
     print(f">>> {cmd}")
-    subprocess.run(cmd, shell=True)
-
+    subprocess.run(cmd, *args, **kw)
 
 def brightness(direction):
     with open(brightness_path, "r") as f:
         cur = int(f.read().strip())
-
-    adj = int(max_brightness * 0.05)  # 5%
+    adj = max_brightness * 5 / 100 # 5% of max brightness
     cur = max(1, min(cur + adj * direction, max_brightness))
-
+    
     with open(brightness_path, "w") as f:
-        f.write(str(cur))
-
+        f.write(f"{int(cur)}\n")
 
 def volume(direction):
-    result = subprocess.run(
-        "amixer get -c 0 DAC | awk -F'[][]' '/Left:/ { print $2 }' | sed 's/%//'",
-        shell=True,
-        capture_output=True,
-        text=True
-    )
-
-    if result.returncode == 0 and result.stdout.strip().isdigit():
-        cur = int(result.stdout.strip())
-        cur = max(0, min(cur + 10 * direction, 100))
-        subprocess.run(f"amixer set -c 0 DAC {cur}%", shell=True)
-
-
-last_lid_state = None
-
+    result = subprocess.run("amixer get -c 0 DAC | awk -F'[][]' '/Left:/ { print $2 }' | sed 's/%//'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print(result)
+    if result.returncode == 0:
+        cur = int(result.stdout)
+        adj = 10
+        cur = max(0, min(cur + adj * direction, 100))
+        re = subprocess.run(f"amixer set -c 0 DAC {cur}%", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print(re)
 
 async def handle_event(device):
-
-    global last_lid_state
-
+    # event.code is the button number
+    # event.value is 1 for press, 0 for release
+    # event.type is 1 for button, 3 for axis
     async for event in device.async_read_loop():
-
-        # ---------------- GAMEPAD ----------------
         if device.name == "H700 Gamepad":
             keys = joypadInput.active_keys()
 
             if Joypad.fn in keys:
-
                 if Joypad.select in keys:
                     if event.code == Joypad.start and event.value == 1:
-                        runcmd("systemctl restart launcher")
-
+                        runcmd("systemctl restart launcher\n", shell=True)
                 if event.code == Joypad.start and event.value == 1:
-                    runcmd("killall retroarch pico8_64 commander simple-terminal fbdoom fbgif-linux-aarch64 || true")
-
+                    runcmd("killall retroarch; killall pico8_64; killall commander; killall simple-terminal; killall fbdoom; killall -INT fbgif-linux-aarch64; true\n", shell=True)
                 if event.code == Joypad.up and event.value == 1:
                     brightness(1)
-
                 if event.code == Joypad.down and event.value == 1:
                     brightness(-1)
-
-        # ---------------- VOLUME ----------------
         elif device.name == "gpio-keys-volume":
             if event.code == 115 and event.value == 1:
                 volume(1)
             if event.code == 114 and event.value == 1:
                 volume(-1)
-
-
-        # ---------------- LID SWITCH ----------------
-        elif device.name == "gpio-keys-lid":
-            if event.type == evdev.ecodes.EV_SW and event.code == evdev.ecodes.SW_LID:
-
-                if event.value != last_lid_state:
-                    last_lid_state = event.value
-
-                    if event.value == 1:
-                        print("LID CLOSED → suspend")
-                        runcmd("systemctl suspend")
-                    else:
-                        print("LID OPENED")
-
+        elif device.name == "axp20x-pek":
+            if event.code == 116 and event.value == 1:
+                print("Power key pressed")
+            #     global suspended
+            #     if suspended == 0:
+            #         suspended = 1
+            #         runcmd("systemctl suspend", shell=True)
+            #     else:
+            #         suspended = 0
+        time.sleep(0.001)
 
 def run():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-
+    
     loop.create_task(handle_event(joypadInput))
     loop.create_task(handle_event(volumeInput))
-    loop.create_task(handle_event(lidInput))
-
+    loop.create_task(handle_event(powerKeyInput))
     loop.run_forever()
 
-
-if __name__ == "__main__":
+if __name__ == "__main__": # admire
     run()
